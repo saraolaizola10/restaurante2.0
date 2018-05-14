7122a1588d97314ff5722f6978b0da37af6a9a9d
saraolaizola <sara.olaizola@opendeusto.es>
2018-05-14 21:26:49 +0200

    nuevos ficheros!

---
+++ LP/frComun.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include "../COMUN/Utilidades.h"
+using namespace std;
+
+void linea()
+{
+    cout << "\n_______________________________________\n" << endl;
+}
+
+
+int introducirOpcion(int opciones)
+{
+    int n;
+
+    cout << "Introduzca opcion (1-" << opciones << "): " << endl;
+    do
+    {
+        n = pedirNumero(0);
+
+        if ((n<0)||(n>opciones))
+        {
+            cout << "Error. Introducir valor valido\n" << endl;
+        }
+    } while ((n<0)||(n>opciones));
+    return n;
+}
---
+++ LP/frComun.h
@@ -0,0 +1,7 @@
+#ifndef _FRCOMUN_H
+#define _FRCOMUN_H
+
+void linea();
+int introducirOpcion(int opciones);
+
+#endif
