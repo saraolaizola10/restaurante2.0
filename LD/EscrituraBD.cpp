b97d230d20c509147d64c218ec242a5a753fe0d2
thecrasher17 <txurfero@gmail.com>
2018-05-17 11:17:45 +0200

    base de datos

--- LD/EscrituraBD.cpp
+++ LD/EscrituraBD.cpp
@@ -17,6 +17,32 @@ void iniciarBD(sqlite3 *db)
   	rc = sqlite3_exec(db, sql_query, 0, 0, &err_message);
   	if (rc != SQLITE_OK ) 
   		cout << sqlite3_errmsg(db);
+
+  	const char *sql_query = "CREATE TABLE PERSONAS (DNI INT PRIMARY KEY NOT NULL, NOMBRE TEXT, APELLIDO TEXT, TEL INT);";
+  	rc = sqlite3_exec(db, sql_query, 0, 0, &err_message);
+  	if (rc != SQLITE_OK ) 
+  		cout << sqlite3_errmsg(db);
+
+  	const char *sql_query = "CREATE TABLE CUENTAS (DNI INT PRIMARY KEY NOT NULL, NOMBRE TEXT, APELLIDO TEXT, TEL INT, SALARIO FLOAT);";
+  	rc = sqlite3_exec(db, sql_query, 0, 0, &err_message);
+  	if (rc != SQLITE_OK ) 
+  		cout << sqlite3_errmsg(db);
+
+  	const char *sql_query = "CREATE TABLE PRODUCTOS (ID INT PRIMARY KEY NOT NULL, NOMBRE TEXT, CATEGORIA TEXT, PRECIO FLOAT);";
+  	rc = sqlite3_exec(db, sql_query, 0, 0, &err_message);
+  	if (rc != SQLITE_OK ) 
+  		cout << sqlite3_errmsg(db);
+
+  	const char *sql_query = "CREATE TABLE CATEGORIAS (ID INT PRIMARY KEY NOT NULL, NOMBRE TEXT, ORDEN INT);";
+  	rc = sqlite3_exec(db, sql_query, 0, 0, &err_message);
+  	if (rc != SQLITE_OK ) 
+  		cout << sqlite3_errmsg(db);
+
+  	const char *sql_query = "CREATE TABLE COMANDAS (DNI INT PRIMARY KEY NOT NULL, FECHYHO INT, TOTAL FLOAT, MEDIA FLOAT);";
+  	rc = sqlite3_exec(db, sql_query, 0, 0, &err_message);
+  	if (rc != SQLITE_OK ) 
+  		cout << sqlite3_errmsg(db);
+
 }
 
 
@@ -53,8 +79,167 @@ int altaCamarero (sqlite3 *db,int dni, string nombre,string apellido, int tel)
 	return SQLITE_OK;
 }
 
-//ALTA CATEGORIA
+int altaPersona (sqlite3 *db,int dni, string nombre,string apellido, int tel) 
+{
+	sqlite3_stmt *stmt;
 
-//ALTA PRODUCTO
+	std::stringstream ss;
+	ss << "INSERT INTO CAMAREROS (dni,nombre,apellido,tel) values (" << dni << ",'" << nombre << "','" << apellido << "'," << tel << ");";
+	std::string ssql = ss.str();
+	char* sql = new char[ssql.length() + 1];
+	strcpy(sql, ssql.c_str());
+	
+	int result = sqlite3_prepare_v2(db,sql,-1,&stmt, NULL) ;
+	if (result != SQLITE_OK) 
+	{
+		cout << sqlite3_errmsg(db) << endl;
+		return result;
+	}
 
-//...
+	result = sqlite3_step(stmt);
+	if (result != SQLITE_DONE) 
+	{
+		cout << "ERROR. El DNI ya existe.\n" << endl;
+		return result;
+	}
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) 
+	{
+		cout << sqlite3_errmsg(db) << endl;
+		return result;
+	}
+	return SQLITE_OK;
+}
+
+int altaCuenta (sqlite3 *db,int dni, string nombre,string apellido, int tel) 
+{
+	sqlite3_stmt *stmt;
+
+	std::stringstream ss;
+	ss << "INSERT INTO CAMAREROS (dni,nombre,apellido,tel,salario) values (" << dni << ",'" << nombre << "','" << apellido << "'," << tel << ",500);";
+	std::string ssql = ss.str();
+	char* sql = new char[ssql.length() + 1];
+	strcpy(sql, ssql.c_str());
+	
+	int result = sqlite3_prepare_v2(db,sql,-1,&stmt, NULL) ;
+	if (result != SQLITE_OK) 
+	{
+		cout << sqlite3_errmsg(db) << endl;
+		return result;
+	}
+
+	result = sqlite3_step(stmt);
+	if (result != SQLITE_DONE) 
+	{
+		cout << "ERROR. El DNI ya existe.\n" << endl;
+		return result;
+	}
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) 
+	{
+		cout << sqlite3_errmsg(db) << endl;
+		return result;
+	}
+	return SQLITE_OK;
+}
+
+int altaProducto (sqlite3 *db,int id, string nombre,string categoria, float precio) 
+{
+	sqlite3_stmt *stmt;
+
+	std::stringstream ss;
+	ss << "INSERT INTO CAMAREROS (id,nombre,categoria,precio) values (" << id << ",'" << nombre << "','" << categoria << "'," << precio << ");";
+	std::string ssql = ss.str();
+	char* sql = new char[ssql.length() + 1];
+	strcpy(sql, ssql.c_str());
+	
+	int result = sqlite3_prepare_v2(db,sql,-1,&stmt, NULL) ;
+	if (result != SQLITE_OK) 
+	{
+		cout << sqlite3_errmsg(db) << endl;
+		return result;
+	}
+
+	result = sqlite3_step(stmt);
+	if (result != SQLITE_DONE) 
+	{
+		cout << "ERROR. El ID ya existe.\n" << endl;
+		return result;
+	}
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) 
+	{
+		cout << sqlite3_errmsg(db) << endl;
+		return result;
+	}
+	return SQLITE_OK;
+}
+
+int altaCategoria (sqlite3 *db,int id, string nombre, int orden) 
+{
+	sqlite3_stmt *stmt;
+
+	std::stringstream ss;
+	ss << "INSERT INTO CAMAREROS (id,nombre,orden) values (" << id << ",'" << nombre << "','" << orden << ");";
+	std::string ssql = ss.str();
+	char* sql = new char[ssql.length() + 1];
+	strcpy(sql, ssql.c_str());
+	
+	int result = sqlite3_prepare_v2(db,sql,-1,&stmt, NULL) ;
+	if (result != SQLITE_OK) 
+	{
+		cout << sqlite3_errmsg(db) << endl;
+		return result;
+	}
+
+	result = sqlite3_step(stmt);
+	if (result != SQLITE_DONE) 
+	{
+		cout << "ERROR. El DNI ya existe.\n" << endl;
+		return result;
+	}
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) 
+	{
+		cout << sqlite3_errmsg(db) << endl;
+		return result;
+	}
+	return SQLITE_OK;
+}
+
+int altaComanda (sqlite3 *db,int dni, int fechayhora, float total, float media) 
+{
+	sqlite3_stmt *stmt;
+
+	std::stringstream ss;
+	ss << "INSERT INTO CAMAREROS (dni,fechayhora,total,media) values (" << dni << ",'" << fechayhora << "','" << total << "'," << media << ");";
+	std::string ssql = ss.str();
+	char* sql = new char[ssql.length() + 1];
+	strcpy(sql, ssql.c_str());
+	
+	int result = sqlite3_prepare_v2(db,sql,-1,&stmt, NULL) ;
+	if (result != SQLITE_OK) 
+	{
+		cout << sqlite3_errmsg(db) << endl;
+		return result;
+	}
+
+	result = sqlite3_step(stmt);
+	if (result != SQLITE_DONE) 
+	{
+		cout << "ERROR. El DNI ya existe.\n" << endl;
+		return result;
+	}
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) 
+	{
+		cout << sqlite3_errmsg(db) << endl;
+		return result;
+	}
+	return SQLITE_OK;
+}
