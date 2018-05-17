00a1a409ed8761ff883d9db34d48ecaf0c4419ff
thecrasher17 <txurfero@gmail.com>
2018-05-17 12:07:27 +0200

    lectura BD

--- LD/LecturaBD.cpp
+++ LD/LecturaBD.cpp
@@ -47,5 +47,76 @@ list <Camarero> getCamareros (sqlite3 *db)
 	return listaCamareros;
 }
 
+list <Persona> getPersona (sqlite3 *db)
+{
+	sqlite3_stmt *stmt;
+	
+	char sql[] = "SELECT DNI,NOMBRE,APELLIDO,TEL, FROM PERSONAS";
+	int result = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) ;
+	if (result != SQLITE_OK) 
+		cout << sqlite3_errmsg(db) << endl;
+
+	list <Persona> listaPersonas;
+	int tel, dni;
+	char str [100];
+	do
+	{
+		result = sqlite3_step(stmt);
+		if (result == SQLITE_ROW) 
+		{
+			dni = sqlite3_column_int(stmt, 0);
+			strcpy(str, (char *) sqlite3_column_text(stmt, 1));
+			string nombre (str);
+			strcpy(str, (char *) sqlite3_column_text(stmt, 2));
+			string apellido (str);
+			tel = sqlite3_column_int(stmt, 3);
+			Persona a (nombre,apellido,tel,dni);
+			listaPersonas.push_back(a);
+		}
+	} while (result == SQLITE_ROW);
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) 
+		cout << sqlite3_errmsg(db) << endl;
+
+	return listaPersonas;
+}
+
+list <Producto> getProducto (sqlite3 *db)
+{
+	sqlite3_stmt *stmt;
+	
+	char sql[] = "SELECT ID,NOMBRE,CATEGORIA,PRECIO FROM PRODUCTOS";
+	int result = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) ;
+	if (result != SQLITE_OK) 
+		cout << sqlite3_errmsg(db) << endl;
+
+	list <Producto> listaProductos;
+	int id;
+	float precio;
+	char str [100];
+	do
+	{
+		result = sqlite3_step(stmt);
+		if (result == SQLITE_ROW) 
+		{
+			id = sqlite3_column_int(stmt, 0);
+			strcpy(str, (char *) sqlite3_column_text(stmt, 1));
+			string nombre (str);
+			strcpy(str, (char *) sqlite3_column_text(stmt, 2));
+			string categoria (str);
+			precio = sqlite3_column_double(stmt,3);
+			Producto a (id,nombre,precio,categoria);
+			listaProductos.push_back(a);
+		}
+	} while (result == SQLITE_ROW);
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) 
+		cout << sqlite3_errmsg(db) << endl;
+
+	return listaProductos;
+}
+
 //GET PRODUCTOS
 //...
