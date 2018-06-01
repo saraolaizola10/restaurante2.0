37220decdce84452780343e4de27b0e6f1b6726a
JOSU <josu.colon@opendeusto.es>
2018-06-01 12:13:00 +0200

    setters admin

--- LN/Administrador.cpp
+++ LN/Administrador.cpp
@@ -27,7 +27,55 @@ ostream &operator<<(ostream &os, const Administrador &a)
 
 istream& operator>> (istream& in, const Administrador &a)
 {
+	string nombre, apellido,cargo;
+	int dni,tel,unica,t;
+	float sueldo;
 
+	cout << "\n\n ** ALTA ADMINISTRADOR **" << endl;
+
+	cin.ignore();
+	cout << "Nombre: " << endl;
+	getline(cin, nombre);
+	cin.clear();
+	transform(nombre.begin(), nombre.end(), nombre.begin(),::toupper);
+	
+	cout << "Apellido: " << endl;
+	getline(cin, apellido);
+	cin.clear();
+	transform(apellido.begin(), apellido.end(), apellido.begin(),::toupper);
+	
+	cout << "DNI (sin letra): " << endl;
+	do
+	{
+		dni = pedirNumero(8);
+		//unica = nuevoCamarero(db,dni);
+		if (unica!=0)
+		{
+			//FALLAAAAA
+			cout << "Error. DNI ya existente. "<<endl;
+		}
+	} while (unica!=0);
+
+	cout << "Telefono:" <<endl;
+	tel = pedirNumero(9);
+
+	cin.ignore();
+	cout << "Cargo:" << endl;
+	getline(cin, cargo);
+	cin.clear();
+	transform(cargo.begin(), cargo.end(), cargo.begin(),::toupper);
+
+	cout << "Sueldo:" << endl;
+	sueldo=pedirFloat();
+	
+	a.setNombre(nombre);
+	a.setApellido(apellido);
+	a.setDni(dni);
+	a.setTel(tel);
+	a.setCargo(cargo);
+	a.setSueldo(sueldo);	
+
+	altaAdministrador(db,dni,nombre,apellido,tel,cargo,sueldo);
 }
 
 void Administrador::diPuesto()
